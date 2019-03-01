1378ed654ba50e655bd35b7fd6f12123e7232b7c
Tayyab <otabz@hotmail.com>
2019-03-01 13:29:06 +1300

    Added in-memory data layer.

--- src/main/java/com/stuff/lightningtalks/Topics.java
+++ src/main/java/com/stuff/lightningtalks/Topics.java
@@ -2,8 +2,23 @@ package com.stuff.lightningtalks;
 
 //business layer
 public class Topics {
+	
+	//no-args, default should be there for injections
+	public Topics(){
+		
+	}
+	
+	//always best to declare dependencies while construction
+	public Topics(DurableTopicsFacade topicsDataStore) {
+		this.topicsDataStore = topicsDataStore;
+	}
+	
+	//inject durable storage
+	DurableTopicsFacade topicsDataStore;
 
 	public boolean submit(String topic, String description, String userId) {
-		return true;
+		return topicsDataStore.persist(topic, description, userId);
 	}
+	
+	
 }
--- src/test/java/com/stuff/lightningtalks/TopicsTest.java
+++ src/test/java/com/stuff/lightningtalks/TopicsTest.java
@@ -1,13 +1,18 @@
 package com.stuff.lightningtalks;
 
+import java.util.List;
+
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class TopicsTest {
+	
 
 	@Test
 	public void submitTopicSuccessfully() {
-		Topics topics = new Topics();
-		assertTrue(topics.submit("First topic", "Topic Description", "email@doamin.com"));
+		Topics topics = new Topics(new DurableTopicsInMemory());
+		topics.submit("First Topic", "Topic Description", "email@domain.com");
 	}
+	
 }
