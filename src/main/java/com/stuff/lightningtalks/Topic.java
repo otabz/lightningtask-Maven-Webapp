d4eb5cd5330c45d436ffa2eb43325222f3bcf62d
Tayyab <otabz@hotmail.com>
2019-03-03 03:49:01 +1300

    Added talking date calculations

--- src/main/java/com/stuff/lightningtalks/Topic.java
+++ src/main/java/com/stuff/lightningtalks/Topic.java
@@ -7,28 +7,29 @@ import java.util.Date;
 //be careful while creating entities from schema
 //business logic must not overwrite
 public class Topic {
-	
+
 	private String subject;
 	private String description;
 	private String userId;
 	private Date time;
-	
+	private Date talkDate;
+
 	// requirements of a valid object must be declared while constructing it
 	public Topic() {
 	}
-	
+
 	public Topic(String subject) {
 		this(subject, "", "", null);
 	}
-	
+
 	public Topic(String subject, String description) {
 		this(subject, description, "", null);
 	}
-	
+
 	public Topic(String subject, String description, String userId) {
 		this(subject, description, userId, null);
 	}
-	
+
 	public Topic(String subject, String description, String userId, Date time) {
 		this.subject = subject;
 		this.description = description;
@@ -36,37 +37,83 @@ public class Topic {
 		Calendar cal = Calendar.getInstance();
 		cal.set(1984, 9, 6);
 		this.time = time == null ? cal.getTime() : time;
+		this.talkDate = calculateTalkDate();
 	}
 	
+	public Date calculateTalkDate() {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(this.time);
+		// year boundary case
+		int submissionMonth = cal.get(Calendar.MONTH);
+		int calculatedMonth = calculateMonth(cal);
+		if(submissionMonth == 11 
+				&& (cal.get(Calendar.MONTH) != calculatedMonth)) {
+			cal.add(Calendar.YEAR, 1);
+		}
+		// month
+		cal.set(Calendar.MONTH, calculatedMonth);
+		// first tuesday
+		Calendar calFromDay1 = Calendar.getInstance();
+		calFromDay1.set(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), 1);
+		cal.set(Calendar.DATE, calculateFirstTuesday(calFromDay1));
+		return cal.getTime();
+	}
+	
+	public int calculateFirstTuesday(Calendar cal) {
+		int day = -1;
+		if(cal.get(Calendar.DAY_OF_WEEK) > 3) {
+			day = 1 + 3 + (7 - cal.get(Calendar.DAY_OF_WEEK));
+		} else {
+			day = 1 + (3 - cal.get(Calendar.DAY_OF_WEEK));
+		}
+		return day;
+//		date + (3 + (7-7))
+//		date + (3 + (7-6))
+//		date + (3 + (7-5))
+//		date + (3 + (7-4))
+	}
+	
+	public int calculateMonth(Calendar cal) {
+		int month = cal.get(Calendar.MONTH) + 1;
+		if(month%2 != 0) {
+			cal.add(Calendar.MONTH, 1);
+		} else {
+			int date = cal.get(Calendar.DATE);
+			Calendar calFromDay1 = Calendar.getInstance();
+			calFromDay1.set(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), 1);
+			int firstTuesday = calculateFirstTuesday(calFromDay1);
+			
+			if(date >= firstTuesday)
+			cal.add(Calendar.MONTH, 2);
+		}
+		return cal.get(Calendar.MONTH);
+	}
+
 	@Override
 	public boolean equals(Object other) {
-		if(null == other) {
+		if (null == other) {
 			return false;
-		}
-		else if(!(other instanceof Topic)) {
+		} else if (!(other instanceof Topic)) {
 			return false;
-		}
-		else if(!((Topic)other).subject.equals(this.subject)) {
+		} else if (!((Topic) other).subject.equals(this.subject)) {
 			return false;
-		}
-		else if(!((Topic)other).description.equals(this.description)) {
+		} else if (!((Topic) other).description.equals(this.description)) {
 			return false;
-		}
-		else if(!((Topic)other).userId.equals(this.userId)) {
+		} else if (!((Topic) other).userId.equals(this.userId)) {
 			return false;
-		}
-		else if(!((Topic)other).time.equals(this.time)) {
+		} else if (((Topic) other).time.compareTo(this.time) != 0) {
+			return false;
+		} else if(((Topic) other).talkDate.compareTo(this.talkDate) != 0) {
 			return false;
 		}
 		return true;
 	}
-	
+
 	@Override
 	public int hashCode() {
-		return this.subject.hashCode()/11
-				+ this.description.hashCode()/33
-				+ this.userId.hashCode()
-				+ (int)this.time.getTime();
+		return this.subject.hashCode() / 11 + this.description.hashCode() / 33
+				+ this.userId.hashCode() + (int) this.time.getTime()
+				+ (int) this.time.getTime();
 	}
 
 }
--- src/test/java/com/stuff/lightningtalks/TopicTest.java
+++ src/test/java/com/stuff/lightningtalks/TopicTest.java
@@ -1,89 +1,172 @@
 package com.stuff.lightningtalks;
 
 import java.util.Calendar;
-
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 public class TopicTest {
-	
+
 	@Test
 	public void whenComparedWithNullMustReturnFalse() {
 		Topic topic = new Topic();
 		assertFalse("Topic comparison with null didn't return false!",
 				topic.equals(null));
 	}
-	
+
 	@Test
 	public void whenComparedWithNotInstanceOfTopicMustReturnFalse() {
 		Topic topic = new Topic();
-		assertFalse("Topic comparison with other object type didn't return false!",
+		assertFalse(
+				"Topic comparison with other object type didn't return false!",
 				topic.equals(new Object()));
 	}
-	
+
 	@Test
 	public void whenComparedToDiffSubjectMustReturnFalse() {
 		Topic topic = new Topic("Topic Subject");
 		Topic other = new Topic("Different Subject");
 		assertFalse("Topic comparison with diff. subject didn't return false!",
 				topic.equals(other));
-		
+
 	}
-	
+
 	@Test
 	public void whenComparedToDiffDescriptionMustReturnFalse() {
 		Topic topic = new Topic("Topic Subject", "Topic Description");
 		Topic other = new Topic("Topic Subject", "Different Description");
-		assertFalse("Topic comparison with diff. description didn't return false!",
+		assertFalse(
+				"Topic comparison with diff. description didn't return false!",
 				topic.equals(other));
 	}
-	
+
 	@Test
 	public void whenComparedToDiffEmailMustReturnFalse() {
 		Topic topic = new Topic("Topic Subject", "Topic Description", "Email");
-		Topic other = new Topic("Topic Subject", "Topic Description", "Different Email");
+		Topic other = new Topic("Topic Subject", "Topic Description",
+				"Different Email");
 		assertFalse("Topic comparison with diff. email didn't return false!",
 				topic.equals(other));
 	}
-	
+
 	@Test
 	public void whenComparedToDiffSubmissionDateMustReturnFalse() {
 		Topic topic = new Topic("Topic Subject", "Topic Description", "Email");
 		Topic other = new Topic("Topic Subject", "Topic Description", "Email",
 				Calendar.getInstance().getTime());
-		assertFalse("Topic comparison with diff. submission date didn't return false!",
+		assertFalse(
+				"Topic comparison with diff. submission date didn't return false!",
 				topic.equals(other));
 	}
-	
+
 	@Test
 	public void whenComparedtoSameTopicMustReturnTrue() {
-		Topic topic = new Topic("Topic Subject", "Topic Description", "user@domain.com",
-				Calendar.getInstance().getTime());
+		Topic topic = new Topic("Topic Subject", "Topic Description",
+				"user@domain.com", Calendar.getInstance().getTime());
 		assertTrue("Topic comparison with same object didn't return true!",
-				topic.equals(new Topic("Topic Subject", "Topic Description", "user@domain.com",
-						Calendar.getInstance().getTime())));
+				topic.equals(new Topic("Topic Subject", "Topic Description",
+						"user@domain.com", Calendar.getInstance().getTime())));
 	}
-	
+
 	@Test
 	public void differentTopicsIdealtoHaveDiffHashCode() {
 		Calendar cal = Calendar.getInstance();
 		cal.add(Calendar.MONTH, 1);
-		Topic topic1 = new Topic("Topic Subject", "Topic Description", "user@domain.com",
-				cal.getTime());
-		Topic topic2 = new Topic("Topic Subject", "Topic Description", "user@domain.com",
-				Calendar.getInstance().getTime());
-		assertFalse("For different topics it is ideal to have different hashcodes!",
+		Topic topic1 = new Topic("Topic Subject", "Topic Description",
+				"user@domain.com", cal.getTime());
+		Topic topic2 = new Topic("Topic Subject", "Topic Description",
+				"user@domain.com", Calendar.getInstance().getTime());
+		assertFalse(
+				"For different topics it is ideal to have different hashcodes!",
 				topic1.hashCode() == topic2.hashCode());
+	}
+
+	@Test
+	public void euqalTopicsMustHaveSameHashCode() {
+		Topic topic1 = new Topic("Topic Subject", "Topic Description",
+				"user@domain.com", Calendar.getInstance().getTime());
+		Topic topic2 = new Topic("Topic Subject", "Topic Description",
+				"user@domain.com", Calendar.getInstance().getTime());
+		assertTrue("Same topics hashcode didn't match!",
+				topic1.hashCode() == topic2.hashCode());
+	}
+
+	@Test
+	public void verifyTalkMonth() {
+		Calendar cal = Calendar.getInstance();
+		cal.set(2019, 0, 28);
+		Topic topic = new Topic("Topic Subject", "Topic Description",
+				"user@domain.com", cal.getTime());
+		assertEquals(1, topic.calculateMonth(cal));
+		
+		cal.set(2019, 10, 28);
+		topic = new Topic("Topic Subject", "Topic Description",
+				"user@domain.com", cal.getTime());
+		assertEquals(11, topic.calculateMonth(cal));
+		
+		cal.set(2019, 01, 4);
+		topic = new Topic("Topic Subject", "Topic Description",
+				"user@domain.com", cal.getTime());
+		assertEquals(1, topic.calculateMonth(cal));
+		
+		cal.set(2019, 01, 5);
+		topic = new Topic("Topic Subject", "Topic Description",
+				"user@domain.com", cal.getTime());
+		assertEquals(3, topic.calculateMonth(cal));
+		
+		cal.set(2019, 11, 3);
+		topic = new Topic("Topic Subject", "Topic Description",
+				"user@domain.com", cal.getTime());
+		assertEquals(1, topic.calculateMonth(cal));
 	}
 	
 	@Test
-	public void euqalTopicsMustHaveSameHashCode() {
-		Topic topic1 = new Topic("Topic Subject", "Topic Description", "user@domain.com",
-				Calendar.getInstance().getTime());
-		Topic topic2 = new Topic("Topic Subject", "Topic Description", "user@domain.com",
-				Calendar.getInstance().getTime());
-		assertTrue("Same topics hashcode didn't match!", topic1.hashCode() == topic2.hashCode());
+	public void verifyFirstTuesdayOfMonth() {
+		Calendar cal = Calendar.getInstance();
+		cal.set(2018, Calendar.DECEMBER, 1);
+		Topic topic = new Topic("Topic Subject", "Topic Description",
+				"user@domain.com", cal.getTime());
+		assertEquals(4, topic.calculateFirstTuesday(cal));
+		
+		cal.set(2019, Calendar.MARCH, 1);
+		topic = new Topic("Topic Subject", "Topic Description",
+				"user@domain.com", cal.getTime());
+		assertEquals(5, topic.calculateFirstTuesday(cal));
+		
+		cal.set(2018, Calendar.NOVEMBER, 1);
+		topic = new Topic("Topic Subject", "Topic Description",
+				"user@domain.com", cal.getTime());
+		assertEquals(6, topic.calculateFirstTuesday(cal));
+		
+		cal.set(2018, Calendar.OCTOBER, 1);
+		topic = new Topic("Topic Subject", "Topic Description",
+				"user@domain.com", cal.getTime());
+		assertEquals(2, topic.calculateFirstTuesday(cal));
+		
+		cal.set(2019, Calendar.SEPTEMBER, 1);
+		topic = new Topic("Topic Subject", "Topic Description",
+				"user@domain.com", cal.getTime());
+		assertEquals(3, topic.calculateFirstTuesday(cal));
 	}
 	
+	@Test
+	public void verifyTalkDateAfter1Month() {
+		Calendar cal = Calendar.getInstance();
+		cal.set(2019, Calendar.JANUARY, 30);
+		Topic topic = new Topic("Topic Subject", "Topic Description",
+				"user@domain.com", cal.getTime());
+		Calendar expected = Calendar.getInstance();
+		expected.set(2019, Calendar.FEBRUARY, 5);
+		assertEquals(0, expected.getTime().compareTo(topic.calculateTalkDate()));
+	}
+	
+	@Test
+	public void verifyTalkDateAfter2Months() {
+		Calendar cal = Calendar.getInstance();
+		cal.set(2019, Calendar.FEBRUARY, 5);
+		Topic topic = new Topic("Topic Subject", "Topic Description",
+				"user@domain.com", cal.getTime());
+		Calendar expected = Calendar.getInstance();
+		expected.set(2019, Calendar.APRIL, 2);
+		assertEquals(0, expected.getTime().compareTo(topic.calculateTalkDate()));
+	}
 }
